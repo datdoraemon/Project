a1015cb2c22415082aceffa2582ff808f5ca5a21
codeproject <datnt.114010121005@vtc.edu.vn>
2022-07-06 23:01:45 +0700

    Project

--- DAL/DBHelper.cs
+++ DAL/DBHelper.cs
@@ -1,40 +1,44 @@
 ﻿using MySql.Data.MySqlClient;
 
-namespace DAL;
-public class DBHelper
+namespace DAL
 {
-    private static MySqlConnection connection;
+    public class DBHelper
+    {
+       private static MySqlConnection connection;
 
-    public static MySqlConnection GetConnection()
-    {
-        if(connection == null)
-        {
-            connection = new MySqlConnection
+       public static MySqlConnection GetConnection()
+       {
+           if(connection == null)
             {
-                ConnectionString = @"server=127.0.0.1;port=3306;user=root;database=Employees;password=562001;"
-            };
-        }
+               connection = new MySqlConnection
+               {
+                   ConnectionString = @"server=127.0.0.1;port=3306;user=root;database=Employees;password=562001;"
+               };
+            }
         return connection;
-    }
-    public static MySqlDataReader ExecQuery(string query)
-    {
-        MySqlCommand sqlCommand = new MySqlCommand(query, connection);
-        return sqlCommand.ExecuteReader();
-    }
-    public static MySqlConnection OpenConnection()
-    {
-        if(connection == null)
-        {
-            GetConnection();
         }
-        connection.Open();
-        return connection;
-    }
-    public static void CloseConnection()
-    {
-        if (connection != null)
+        public static MySqlDataReader ExecQuery(string query)
         {
-            connection.Close();
+          MySqlCommand sqlCommand = new MySqlCommand(query, connection);
+          return sqlCommand.ExecuteReader();
+        }
+        public static MySqlConnection OpenConnection()
+        {
+          if(connection == null)
+          {
+              GetConnection();
+          }
+          connection.Open();
+          return connection;
+        }
+        public static void CloseConnection()
+        {
+           if (connection != null)
+           {
+                connection.Close();
+           }
         }
     }
 }
+
+
