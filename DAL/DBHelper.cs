3be4dbc482d73760fc05b9627b203d1d8219692a
codeproject <datnt.114010121005@vtc.edu.vn>
2022-07-06 21:50:26 +0700

    Project

---
+++ DAL/DBHelper.cs
@@ -0,0 +1,40 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace DAL;
+public class DBHelper
+{
+    private static MySqlConnection connection;
+
+    public static MySqlConnection GetConnection()
+    {
+        if(connection == null)
+        {
+            connection = new MySqlConnection
+            {
+                ConnectionString = @"server=127.0.0.1;port=3306;user=root;database=Employees;password=562001;"
+            };
+        }
+        return connection;
+    }
+    public static MySqlDataReader ExecQuery(string query)
+    {
+        MySqlCommand sqlCommand = new MySqlCommand(query, connection);
+        return sqlCommand.ExecuteReader();
+    }
+    public static MySqlConnection OpenConnection()
+    {
+        if(connection == null)
+        {
+            GetConnection();
+        }
+        connection.Open();
+        return connection;
+    }
+    public static void CloseConnection()
+    {
+        if (connection != null)
+        {
+            connection.Close();
+        }
+    }
+}
---
+++ Persistence/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace Persistence;
+public class Order
+{
+    public int OrderId {get; set;}
+    public DateTime Dates {get; set;}
+    public string Payment_Method {get; set;}
+    public int Customer_ID {get; set;}
+    public int SalesmanID {get; set;}
+    public int DishID {get; set;}
+    public int Quantity {get; set;}
+
+}
